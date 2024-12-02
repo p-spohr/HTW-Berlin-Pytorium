3de5ec109d0b66cc6fd5515ee80393f9f72b3925
Patrick <spohrpatrick@gmail.com>
2024-12-02 15:58:48 +0100

    lecture 02.12.24

--- mpl_test.py
+++ mpl_test.py
@@ -1,16 +1,14 @@
 # %%
-
 import matplotlib.pyplot as plt
 
 # %%
-
 # normale Methode um ein Figure zu initialisieren
-fig, ax = plt.subplots(2,2)
+fig, axs = plt.subplots(2,2)
 
 # %%
 
-fig
-
 # %%
 
-fig.savefig('2mal2fig.png')
+fig.savefig('2mal2fig.png', format='png')
+
+# %%
